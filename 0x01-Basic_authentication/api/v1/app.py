24c0914a4e535e8d231617d0ac1a1461cbaffbb6
eyasu <daa282386@gmail.com>
2023-08-08 11:01:51 +0300

    handling errors

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,7 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 import os
 
 
@@ -14,10 +14,21 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.errorhandler(401)
+def unauthorized(error) -> dict:
+    """Unauthorized error handler"""
+    return jsonify({"error": "unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def unauthorized(error) -> dict:
+    """Forbidden error handler"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
