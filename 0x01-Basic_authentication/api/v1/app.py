8db17721520e4da31a0ed1c92967842cb06678e8
eyasu <daa282386@gmail.com>
2023-08-08 17:22:26 +0300

    basic_authwentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,16 +2,41 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import CORS, cross_origin
 import os
+from os import getenv
 
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.views import app_views
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+auth_type = getenv("AUTH_TYPE", "auth")
+if auth_type == "auth":
+    auth = Auth()
+if auth_type == "basic_auth":
+    auth = BasicAuth()
+
+
+@app.before_request
+def authenticate_user():
+    paths = [
+        "/api/v1/status/",
+        "/api/v1/unauthorized/",
+        "/api/v1/forbidden/",
+    ]
+    if auth is None:
+        return
+    if auth.require_auth(request.path, paths):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
 
 
 @app.errorhandler(401)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,20 +3,29 @@
 Authentication module for the API
 """
 
-from flask import request
+import fnmatch
 from typing import List, TypeVar
+
+from flask import request
 
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """return Flase"""
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """request: the flask request object
         return None
         """
-        return None
+        if request is None or not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar("User"):
         """request: the flask request object
